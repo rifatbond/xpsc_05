d98cb17de15562c9d160e12649ed2207298e0ee5
rifatbond <165579070+rifatbond@users.noreply.github.com>
2024-11-08 23:30:49 +0600

    Add files via upload

---
+++ week 2/day 1/day 7/C_Train_and_Queries.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int t;
+   cin>>t;
+   while (t--)
+    {
+      int n, m;
+      cin>>n>>m;
+      map<int,set<int>> mp;
+      for (int i=1;i<= n;i++) 
+      {
+         int x;
+         cin >> x;
+         mp[x].insert(i);
+      }
+
+      for (int i = 1;i <= m;i++)
+       {
+         int l, r;
+         cin >> l >> r;
+         if((mp.find(l)==mp.end()) || (mp.find(r)==mp.end()))
+         {
+            cout<<"NO"<<endl;
+         }
+         else
+         {
+            int first,second;
+            first=*mp[l].begin();
+            second=*mp[r].rbegin();
+            if(first<second)
+            {
+                cout<<"YES"<<endl;
+            }
+            else
+            {
+                cout<<"NO"<<endl;
+            }
+         }
+         
+        
+      }
+   }
+
+   return 0;
+}
